8ae0410e8ad6b7117be080028c628de206ebe2d7
吴俊龙 <735626035@qq.com>
2018-09-21 10:59:24 +0800

    双色球

--- src/main/resources/static/assets/js/wjl.js
+++ src/main/resources/static/assets/js/wjl.js
@@ -1,33 +1,4 @@
-$(document).ready(function () {
+$(function () {
+    NProgress.configure({ easing: 'ease', speed: 500, showSpinner: false })
     NProgress.start();
-    $('.btn-close').click(function (e) {
-        e.preventDefault();
-        e.stopPropagation();
-        $(this).parent().parent().parent().fadeOut();
-    });
-    $('.btn-minimize').click(function (e) {
-        e.preventDefault();
-        e.stopPropagation();
-        var $target = $(this).parent().parent().next('.box-content');
-        if ($target.is(':visible')){
-            $('i', $(this)).removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
-        }
-        else{
-            $('i', $(this)).removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
-        }
-        $target.slideToggle();
-    });
-
-    $("div.box-header").click(function(e){
-        e.preventDefault();
-        e.stopPropagation();
-        var $target = $(this).next('.box-content');
-        if ($target.is(':visible')){
-            $('i', $("a.btn-minimize")).removeClass('glyphicon-chevron-up').addClass('glyphicon-chevron-down');
-        }
-        else{
-            $('i', $("a.btn-minimize")).removeClass('glyphicon-chevron-down').addClass('glyphicon-chevron-up');
-        }
-        $target.slideToggle();
-    })
-});
+})
