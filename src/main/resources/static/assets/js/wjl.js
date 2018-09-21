c04c2e54ae465404c971479bdac96b20673c1b64
吴俊龙 <735626035@qq.com>
2018-09-21 11:46:02 +0800

    双色球

--- src/main/resources/static/assets/js/wjl.js
+++ src/main/resources/static/assets/js/wjl.js
@@ -2,3 +2,28 @@ $(function () {
     NProgress.configure({ easing: 'ease', speed: 500, showSpinner: false })
     NProgress.start();
 })
+$(window).load(function(){
+    NProgress.done();
+})
+$(window).ajaxStart(function () {
+    NProgress.start();
+});
+
+$(window).ajaxStop(function () {
+    NProgress.done();
+});
+axios.interceptors.request.use(function(config){
+    NProgress.start();
+    return config;
+},function(err){
+    NProgress.done();
+    return Promise.reject(error);
+});
+//添加一个响应拦截器
+axios.interceptors.response.use(function(res){
+    NProgress.done();
+    return res;
+},function(err){
+    NProgress.done();
+    return Promise.reject(error);
+})
